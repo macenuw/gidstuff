3a9182a1a177c50f3e706aa715d1e26f44f15c46
macenuw <gnykabiny54@gmail.com>
2021-03-09 12:49:39 +0200

    first comit

--- app/js/main.js
+++ app/js/main.js
@@ -12,6 +12,25 @@ $(function () {
         slidesToShow: 6,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 3000,
+        autoplaySpeed: 5000,
+        responsive: [{
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 5,
+                }
+            },
+            {
+                breakpoint: 1060,
+                settings: {
+                    slidesToShow: 4,
+                }
+            },
+            {
+                breakpoint: 640,
+                settings: {
+                    slidesToShow: 3,
+                }
+            }
+        ]
     });
 });
